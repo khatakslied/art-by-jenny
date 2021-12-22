3ff987b12bab34bb470d9d06406fa49f841b9d44
Ryan Johnson <ryan.a.y.johnson@gmail.com>
2021-12-22 12:36:01 +0900

    Add responsive styling to Basket.jsx.

--- src/pages/Basket.jsx
+++ src/pages/Basket.jsx
@@ -20,36 +20,6 @@ const Title = styled.h2`
 
 const Top = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: space-between;
-  margin: 16px 0;
-`;
-
-const TopButton = styled.button`
-  border: none;
-  border-radius: 1px;
-  padding: 8px;
-  font-size: 1rem;
-  background-color: #412D26;
-  color: #FEFEFE;
-  cursor: pointer;
-  box-shadow: 0 0 1px #412D26;
-  white-space: nowrap;
-  transition: 0.5s;
-
-  &:hover{
-    background-color: #161413;
-    transform: scale(1.1)
-  }
-`;
-
-const TopText = styled.div`
-  text-decoration: underline;
-  cursor: pointer;
-`;
-
-const Bottom = styled.div`
-  display: flex;
   justify-content: space-between;
   margin: 16px 0;
   @media (max-width: 768px) {
@@ -123,11 +93,25 @@ const Summary = styled.div`
   align-items: flex-end;
   justify-content: center;
   @media (max-width: 768px) {
+    flex-direction: row;
+    flex-wrap: wrap;
     align-items: center;
   }
 `;
 
-const SummaryTitle = styled.h3`
+const SummaryContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: center;
+`;
+
+const Items = styled.div`
+  text-decoration: underline;
+  cursor: pointer;
+`;
+
+const SummaryKey = styled.h3`
   padding: 8px;
   font-size: 1rem;
   font-weight: normal;
@@ -135,6 +119,32 @@ const SummaryTitle = styled.h3`
 
 const Total = styled.h3`
   padding: 8px;
+`;
+
+
+const Bottom = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 16px 0;
+`;
+
+const Button = styled.button`
+  border: none;
+  border-radius: 1px;
+  padding: 8px;
+  font-size: 1rem;
+  background-color: #412D26;
+  color: #FEFEFE;
+  cursor: pointer;
+  box-shadow: 0 0 1px #412D26;
+  white-space: nowrap;
+  transition: 0.5s;
+
+  &:hover{
+    background-color: #161413;
+    transform: scale(1.1)
+  }
 `;
 
 const Basket = () => {
@@ -145,11 +155,6 @@ const Basket = () => {
       <Wrapper>
         <Title>Basket</Title>
         <Top>
-          <TopButton>Continue Shopping</TopButton>
-          <TopText>Items (0)</TopText>
-          <TopButton>Checkout</TopButton>
-        </Top>
-        <Bottom>
           <Info>
             <Product>
               <Image src={testImage} />
@@ -171,13 +176,24 @@ const Basket = () => {
             </Product>
           </Info>
           <Summary>
-            <SummaryTitle>Subtotal</SummaryTitle>
-            <Total>$100</Total>
-            <SummaryTitle>Postage</SummaryTitle>
-            <Total>$10</Total>
-            <SummaryTitle>Total</SummaryTitle>
-            <Total>$110</Total>
+            <Items>Items (0)</Items>
+            <SummaryContainer>
+              <SummaryKey>Subtotal</SummaryKey>
+              <Total>$100</Total>
+            </SummaryContainer>
+            <SummaryContainer>
+              <SummaryKey>Postage</SummaryKey>
+              <Total>$10</Total>
+            </SummaryContainer>
+            <SummaryContainer>
+              <SummaryKey>Total</SummaryKey>
+              <Total>$110</Total>
+            </SummaryContainer>
           </Summary>
+        </Top>
+        <Bottom>
+          <Button>Continue Shopping</Button>
+          <Button>Checkout</Button>
         </Bottom>
       </Wrapper>
       <Footer />
